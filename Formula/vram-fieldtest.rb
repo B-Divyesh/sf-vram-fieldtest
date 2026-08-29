95978e5b31a39a8449545fd0bdd6272c04efeb07
Param Factory <factory@sociobot.in>
2026-08-29 20:11:34 +0000

    release: finalize v0.1.4 package manifests

--- Formula/vram-fieldtest.rb
+++ Formula/vram-fieldtest.rb
@@ -4,10 +4,10 @@ class VramFieldtest < Formula
   version "0.1.4"
   if Hardware::CPU.arm?
     url "https://github.com/B-Divyesh/sf-vram-fieldtest/releases/download/v0.1.4/vram-fieldtest-macos-aarch64.tar.gz"
-    sha256 "336e3243293bb965909423b60290f0cc4566c65b8fa2df7d75197effbbcc1e1d"
+    sha256 "6b31086f12e57712e287fd4ffc314e3c968c317f0d43008fc9b2ec1bd0831cb1"
   else
     url "https://github.com/B-Divyesh/sf-vram-fieldtest/releases/download/v0.1.4/vram-fieldtest-macos-x86_64.tar.gz"
-    sha256 "b076d00a6338621d4d7c0292f43768e095140f3ba85867ebdc0190ba2cdb9e58"
+    sha256 "d3ee4a29db4da372c2d938fe42e65d86f29d0d11b3a2f7f2776a37a9a1fc11ce"
   end
   def install
     bin.install "vram-fieldtest"
